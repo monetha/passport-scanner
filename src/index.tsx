4e9b7b30333265191500d80e2870e1f1aae61dd3
Martynas <martynas.adomaitis@monetha.io>
2019-05-13 16:35:36 +0300

    Fonts, style vars

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,25 @@ import { bootstrapIOC } from './ioc/bootstrapIOC';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 
+// Load favicon
 require('images/favicon.ico');
+
+// Load fonts
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Medium.ttf';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Medium.woff2';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Medium.woff';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Light.ttf';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Light.woff2';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Light.woff';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Italic.ttf';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Italic.woff2';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Italic.woff';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Regular.ttf';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Regular.woff2';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Regular.woff';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Bold.ttf';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Bold.woff2';
+import '!file-loader?name=[name].[ext]&outputPath=assets/fonts/!./assets/fonts/CerebriSans-Bold.woff';
 
 const history = createBrowserHistory();
 const store = bootstrapRedux({}, history);
