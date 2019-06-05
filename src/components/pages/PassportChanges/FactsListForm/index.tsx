51997e4f3ce65de48e3ab654e8b9868b75f924b7
Alexander Hitrov <alexander.hitrov@monetha.io>
2019-06-05 12:51:42 +0300

    col margin.

--- src/components/pages/PassportChanges/FactsListForm/index.tsx
+++ src/components/pages/PassportChanges/FactsListForm/index.tsx
@@ -104,7 +104,7 @@ class FactsListForm extends React.PureComponent<IProps> {
     return (
       <Form>
         <Row>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} className='col'>
             <FormikField
               name='passportAddress'
               label={translate(t => t.form.passportAddress)}
@@ -119,7 +119,7 @@ class FactsListForm extends React.PureComponent<IProps> {
             </FormikField>
           </Col>
 
-          <Col xs={12} md={2}>
+          <Col xs={12} md={2} className='col'>
             <FormikField
               name='startBlock'
               label={translate(t => t.form.startBlock)}
@@ -133,7 +133,7 @@ class FactsListForm extends React.PureComponent<IProps> {
             </FormikField>
           </Col>
 
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} className='col'>
             <FormikField
               name='factProvider'
               label={translate(t => t.passport.factProvider)}
@@ -147,7 +147,7 @@ class FactsListForm extends React.PureComponent<IProps> {
             </FormikField>
           </Col>
 
-          <Col xs={12} md={2}>
+          <Col xs={12} md={2} className='col'>
             <div className='mh-form-buttons'>
               <Button
                 type='submit'
